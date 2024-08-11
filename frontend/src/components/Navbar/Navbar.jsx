7a5c343f6098d9c60e8f8cd1dc85e854cd57473d
Tharushika Sadan <143350661+SadanTharu@users.noreply.github.com>
2024-08-11 11:30:29 +0530

    Frontend initiate with Navbar

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <p>Navbar</p>
+    </div>
+  )
+}
+
+export default Navbar
